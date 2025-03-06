61f8fa5419544e1103b255d00b4bed73e30fa6cc
amir-khaksar <=>
2025-03-06 19:13:23 +0330

    fix header bug

--- app/src/Components/Header/Header.tsx
+++ app/src/Components/Header/Header.tsx
@@ -49,37 +49,42 @@ const Header = () => {
                 ></div>
             }
 
-            <span className="absolute bottom-0 right-0 left-0 mx-auto text-secondary dark:text-bgDark w-[100px] h-[22]">
-        <svg
-            className="text-secondary dark:text-bgDark"
-            width="100"
-            height="22"
-            viewBox="0 0 100 22"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-              d="M50 0C69 0 81 22 100 22L0 22C18.75 22 31 0 50 0Z"
-              fill="currentColor"
-          />
-        </svg>
-      </span>
-            <span className="absolute bottom-0 right-0 left-1 mx-auto w-5 h-5">
-        <svg
-            className="w-6 h-6 text-black dark:text-secondary"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-        >
-          <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="m19.5 8.25-7.5 7.5-7.5-7.5"
-          />
-        </svg>
-      </span>
+            {
+                isMobile ? "" : <>
+                    <span
+                        className="absolute bottom-0 right-0 left-0 mx-auto text-secondary dark:text-bgDark w-[100px] h-[22]">
+                <svg
+                    className="text-secondary dark:text-bgDark"
+                    width="100"
+                    height="22"
+                    viewBox="0 0 100 22"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                        d="M50 0C69 0 81 22 100 22L0 22C18.75 22 31 0 50 0Z"
+                        fill="currentColor"
+                    />
+                </svg>
+            </span>
+                    <span className="absolute bottom-0 right-0 left-1 mx-auto w-5 h-5">
+                <svg
+                    className="w-6 h-6 text-black dark:text-secondary"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="m19.5 8.25-7.5 7.5-7.5-7.5"
+                    />
+                </svg>
+            </span>
+                </>
+            }
             <Landing/>
         </div>
     );
